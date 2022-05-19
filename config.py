5af9c1327d51b131e77e0e3cebc2522ae6f37d54
Wamaitha <wamaithaweru19@gmail.com>
2022-05-20 01:12:03 +0300

    add emailurl to config

--- config.py
+++ config.py
@@ -1,10 +1,18 @@
 import os
 # parent Config class contains configurations that are used in both production and development stages.
 class Config:
-    '''
-    General configuration parent class
-    '''
-    pass
+    SECRET_KEY = 'Black'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://tours:2022@localhost/safari'
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+    # mail
+    MAIL_SERVER='smtp.gmail.com'
+    MAIL_PORT = 465
+    # update it with your gmail
+    MAIL_USERNAME = 'your_mail@gmail.com'
+    # update it with your password
+    MAIL_PASSWORD= 'your_email_password'
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
 
 
 # ProdConfig subclass contains configurations that are used in production stages of our application and inherits from the parent Config class.
